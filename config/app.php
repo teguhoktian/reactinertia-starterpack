149b4bbda33ce5f27ebcd5b573bdedfa50eb0f11
Hoki Teguh Oktian <oktian89@gmail.com>
2023-06-14 20:49:49 +0700

    Instalasi Laravel Permission by Spatie

--- config/app.php
+++ config/app.php
@@ -168,6 +168,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        //Spatie Laravel Permission
+        Spatie\Permission\PermissionServiceProvider::class,
     ])->toArray(),
 
     /*
