cc7310a525e984ff2f828d85ce2ad5ca372507fa
Hoki Teguh Oktian <oktian89@gmail.com>
2023-06-17 11:08:05 +0700

    Remove Header Card When empty

--- resources/js/MyComponents/Card/Card.jsx
+++ resources/js/MyComponents/Card/Card.jsx
@@ -5,10 +5,12 @@ function Card({ header, description, children }) {
         <>
             <div className="flex flex-col border border-gray-300 bg-white">
                 {/* Header */}
-                <div className="border-b bg-gray-100 border-gray-300 md:px-8 px-4 py-4 flex flex-col">
-                    <strong>{header}</strong>
-                    {description && <small>{description}</small>}
-                </div>
+                {header && (
+                    <div className="border-b bg-gray-100 border-gray-300 md:px-8 px-4 py-4 flex flex-col">
+                        <strong>{header}</strong>
+                        {description && <small>{description}</small>}
+                    </div>
+                )}
                 {/* Body */}
                 <div className="p-4 md:p-8">{children}</div>
             </div>
