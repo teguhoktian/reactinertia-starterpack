a49293487936e1c8c4d7456808273450f54a651a
Hoki Teguh Oktian <oktian89@gmail.com>
2023-06-19 21:28:46 +0700

    Add Middleware Spatie Permissions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,6 +15,7 @@ class UserController extends Controller
     public function __construct(UserService $services)
     {
         $this->services = $services;
+        $this->middleware(['role:Admin']);
     }
 
     /**
