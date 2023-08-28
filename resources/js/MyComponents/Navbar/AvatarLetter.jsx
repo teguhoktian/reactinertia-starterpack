62e18de54f782106be7b8abe541aeb3d8205e703
Hoki Teguh Oktian <oktian89@gmail.com>
2023-08-28 08:44:55 +0700

    Perbaikan class pada AvatarLetter Component

--- resources/js/MyComponents/Navbar/AvatarLetter.jsx
+++ resources/js/MyComponents/Navbar/AvatarLetter.jsx
@@ -33,8 +33,8 @@ function stringAvatar(name) {
 function AvatarLetter({ name }) {
     return (
         <>
-            <div class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-                <span class="font-medium text-gray-600 dark:text-gray-300">
+            <div className="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                <span className="font-medium text-gray-600 dark:text-gray-300">
                     {`${name.split(" ")[0][0]}${name.split(" ")[1][0]}`}
                 </span>
             </div>
