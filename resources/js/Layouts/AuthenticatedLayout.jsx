0709e1a30e009800913ae3fc8547fd45750d3e35
Hoki Teguh Oktian <oktian89@gmail.com>
2023-10-08 19:08:43 +0700

    Minor Update for Logo

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import ApplicationLogo from "@/Components/ApplicationLogo";
 import Dropdown from "@/Components/Dropdown";
 import NavLink from "@/Components/NavLink";
 import ResponsiveNavLink from "@/Components/ResponsiveNavLink";
 import { Link } from "@inertiajs/react";
+import { Square3Stack3DIcon } from "@heroicons/react/24/solid";
 
 export default function Authenticated({ user, header, children }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] =
@@ -17,7 +17,7 @@ export default function Authenticated({ user, header, children }) {
                         <div className="flex">
                             <div className="shrink-0 flex items-center">
                                 <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
+                                    <Square3Stack3DIcon className="w-auto h-9 text-indigo-600" />
                                 </Link>
                             </div>
 
