e72ae05b5c9e9174b6a23e72d74d91badfce88e9
Hoki Teguh Oktian <oktian89@gmail.com>
2023-10-15 10:41:00 +0700

    Perbaikan pada Profile Dropdown Responsive

--- resources/js/MyComponents/Navbar/AvatarLetter.jsx
+++ resources/js/MyComponents/Navbar/AvatarLetter.jsx
@@ -34,7 +34,7 @@ function AvatarLetter({ name }) {
     return (
         <>
             <div className="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-                <span className="font-medium text-gray-600 dark:text-gray-300">
+                <span className="font-medium text-gray-600 dark:text-gray-300 cursor-pointer">
                     {`${name.split(" ")[0][0]}${name.split(" ")[1][0]}`}
                 </span>
             </div>
--- resources/js/MyComponents/Navbar/TopNavbar.jsx
+++ resources/js/MyComponents/Navbar/TopNavbar.jsx
@@ -34,7 +34,7 @@ function TopNavbar({ auth, header, ...props }) {
                                 />
                                 <button
                                     type="button"
-                                    className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                    className="hidden md:inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
                                 >
                                     Hi, {auth.user.firstname}
                                     <ChevronDownIcon className="w-4 h-4 ml-2" />
