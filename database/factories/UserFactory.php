aec46c5f73d463dc9a34541cd7c691fcfc143f89
Hoki Teguh Oktian <oktian89@gmail.com>
2023-12-06 09:40:02 +0700

    Perbaikan User Factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,8 +18,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'firstname' => fake()->firstName(),
-            'lastname' => fake()->lastName(),
+            'name' => fake()->name(),
+            // 'lastname' => fake()->lastName(),
             'username' => fake()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
