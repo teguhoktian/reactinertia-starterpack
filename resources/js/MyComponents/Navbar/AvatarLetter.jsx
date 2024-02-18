43066d6d94256296914cb639848c4d5209687f80
Hoki Teguh Oktian <oktian89@gmail.com>
2024-02-18 09:19:28 +0700

    Avatar Letter Update

--- resources/js/MyComponents/Navbar/AvatarLetter.jsx
+++ resources/js/MyComponents/Navbar/AvatarLetter.jsx
@@ -35,11 +35,11 @@ function AvatarLetter({ name, className = "w-10 h-10 font-medium" }) {
         <>
             <div
                 className={
-                    `relative inline-flex items-center justify-center overflow-hidden bg-indigo-50 rounded-full  border-gray-300 border cursor-pointer ` +
+                    `relative inline-flex items-center justify-center overflow-hidden bg-indigo-50 rounded-full  border-gray-200 border cursor-pointer ` +
                     className
                 }
             >
-                <span className=" text-gray-600 dark:text-indigo-300 text-base cursor-pointer">
+                <span className=" text-gray-700 dark:text-indigo-400 text-base cursor-pointer">
                     {`${name.split(" ")[0][0]}${name.split(" ")[1][0]}`}
                 </span>
             </div>
